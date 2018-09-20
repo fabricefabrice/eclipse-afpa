f5ec3700938e34ef004e4ebe0b5d6ea067e6557e
59013-64-09 <59013-64-09@STA6100529.exchange.ad.afpanet>
2018-09-20 16:24:34 +0200

    Animaux

---
+++ Animaux/src/Main.java
@@ -0,0 +1,60 @@
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+
+
+		Chien ch1 = new Chien ()  ;
+		ch1.nom = "ch1"  ;
+		ch1.taille = 100 ;
+		ch1.poids = 25 ;
+		ch1.couleur =  "noire";
+		ch1.race = "berger"  ;
+
+
+
+
+		System.out.println("Voici le chien : " + ch1.nom);
+		System.out.println("qui mesure  : " + ch1.taille);
+		System.out.println("poids : " + ch1.poids)  ;
+		System.out.println("de couleur : " + ch1.couleur);
+		System.out.println("race : " + ch1.race)  ;
+
+
+		Chat ct1 = new Chat("ct1", 50, 5 , "noir" , "angora")  ;
+	
+
+		System.out.println("Voici le chat : " + ct1.nom);
+		System.out.println("qui mesure  : " + ct1.taille);
+		System.out.println("poids : " + ct1.poids)  ;
+		System.out.println("de couleur : " + ct1.couleur);
+		System.out.println("race : " + ct1.race)  ;
+		
+		ct1.miauler();
+
+		Chien ada = new Chien ("ada" , 152, 5, "noir", "levrier")  ;
+		System.out.println("la couleur du chien" + ada.nom  + "est le : " + ada.couleur);
+
+		Chien rex = new Chien ("rex ", 120, 2, "rouge", "bulldog") ;
+		System.out.println("le chien" + rex.nom  + "est un : " + rex.race + "de poids :" +rex.poids);
+
+		Animal monanimal = new Chien("chien sans nom", 10, 1, "gris", "Teckel") ;
+				System.out.println( monanimal.nom + "est de couleur : " + monanimal.couleur);
+		
+
+		
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
